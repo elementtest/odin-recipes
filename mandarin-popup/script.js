6c8deca9e601ff02b2a8010588b4a71d18e6992f
elementtest <elementtest8@gmail.com>
2026-04-27 16:14:10 +0800

    add project mandarin character tiles

---
+++ mandarin-popup/script.js
@@ -0,0 +1,12 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("mouseenter", () => {
+    panel.classList.add("active");
+
+    panel.addEventListener("mouseleave", () => {
+      panel.classList.remove("active");
+    });
+  });
+});
+
