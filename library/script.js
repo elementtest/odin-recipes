222bc4df9cb678ee235a7ce7a8c0f1884e1e59fd
elementtest <elementtest8@gmail.com>
2026-06-03 17:32:02 +0800

    variables for each from the submitted form

--- library/script.js
+++ library/script.js
@@ -1,6 +1,7 @@
 const dialogElem = document.getElementById("dialog");
 const showBtn = document.querySelector(".show");
 const closeBtn = document.querySelector(".close");
+const form = document.getElementById("book-form");
 
 showBtn.addEventListener("click", () => {
   dialogElem.showModal();
@@ -9,3 +10,15 @@ showBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   dialogElem.close();
 });
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.querySelector('input[name="read"]:checked').value;
+  console.log(title , author , pages, read);
+  dialogElem.close();
+  form.reset();
+})
+
