eb5ee2e9d72ed9fc4395dc6f37cecc27c43641c2
elementtest <elementtest8@gmail.com>
2026-06-04 09:04:08 +0800

    add removeBtn class

--- library/script.js
+++ library/script.js
@@ -11,18 +11,37 @@ closeBtn.addEventListener("click", () => {
   dialogElem.close();
 });
 
-form.addEventListener('submit', (e) => {
+
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const pages = document.getElementById('pages').value;
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
   const read = document.querySelector('input[name="read"]:checked').value;
   let uniqueid = crypto.randomUUID();
-  const myLibrary = [{title, author, pages, read, uniqueid}];
-  console.log(myLibrary);
+  const book = { title, author, pages, read, uniqueid };
+  console.log(book);
+  displaybook(book);
+
   // close after submit
   // dialogElem.close();
   // reset form after submit
   // form.reset();
-})
+});
+function displaybook(book) {
+  const bottom = document.querySelector(".bottom");
 
+  const tile = document.createElement("div");
+  tile.classList.add("book-tile");
+  tile.dataset.id = book.uniqueid;
+
+  tile.innerHTML = `
+      <h3 class="title">${book.title}</h3>
+      <h3 class="author">${book.author}</h3>
+      <h3 class="pages">${book.pages}</h3>
+      <h3 class="read">${book.read === "read" ? "READ" : "NOT READ"}</h3>
+      <button>remove</button>
+      `;
+
+  bottom.appendChild(tile);
+}
